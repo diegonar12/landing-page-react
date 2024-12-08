b9fb7aab371e79f94d2096a39daf9c7099994d4d
diegonar12 <diegonmartin12@gmail.com>
2024-12-08 23:06:22 +0000

    correciones

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,22 @@
-import Navbar from "./navbar";
+import Navbar from './Navbar';
 import Jumbotron from "./Jumbotron";
 import Cards from "./Cards";
 import Footer from "./Footer";
 
-const home = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="container mt-3 mb-3">
-        <Jumbotron />
-        <div className="mb-3"></div> 
-        <Cards />
-      </div>
-      <Footer />
-    </div>
-  );
-};
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <Navbar />
+                <div className="container mt-3 mb-3">
+                    <Jumbotron />
+                    <div className="mb-3"></div>
+                    <Cards />
+                </div>
+                <Footer />
+            </div>
+        </div>
+    )
+}
 
-export default home;
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './home'
-
+import Home from './Home'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Home/>
   </StrictMode>,
 )
