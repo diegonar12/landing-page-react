b50cc76a09f7b3de7a4faee62d205feef80ecafe
diegonar12 <diegonmartin12@gmail.com>
2024-12-09 23:10:07 -0500

    correciones grid system y map de cards

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,88 +1,57 @@
 const Cards = () => {
+  const cardsData = [
+    {
+      id: 1,
+      title: "Card 1",
+      text: "This is the content of the first card.",
+      imgSrc:
+        "https://raw.githubusercontent.com/R-CoderDotCom/samples/main/bird.png",
+    },
+    {
+      id: 2,
+      title: "Card 2",
+      text: "This is the content of the second card.",
+      imgSrc:
+        "https://raw.githubusercontent.com/R-CoderDotCom/samples/main/bird.png",
+    },
+    {
+      id: 3,
+      title: "Card 3",
+      text: "This is the content of the third card.",
+      imgSrc:
+        "https://raw.githubusercontent.com/R-CoderDotCom/samples/main/bird.png",
+    },
+    {
+      id: 4,
+      title: "Card 4",
+      text: "This is the content of the fourth card.",
+      imgSrc:
+        "https://raw.githubusercontent.com/R-CoderDotCom/samples/main/bird.png",
+    },
+  ];
+
   return (
     <div
-      className="d-flex justify-content-between align-items-start flex-wrap"
+      className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2"
       style={{ margin: "0 auto", maxWidth: "1200px", padding: "20px" }}
     >
-      <div
-        className="card"
-        style={{ width: "18rem", flex: "1", margin: "0 10px" }}
-      >
-        <img
-          className="card-img-top"
-          src="/src/files /IMG_0166.HEIC"          alt="Card image cap"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+      {cardsData.map((card) => (
+        <div className="col" key={card.id}>
+          <div
+            className="card"
+            style={{ margin: "0", boxSizing: "border-box" }} 
+          >
+            <img className="card-img-top" src={card.imgSrc} alt={card.title} />
+            <div className="card-body">
+              <h5 className="card-title">{card.title}</h5>
+              <p className="card-text">{card.text}</p>
+              <a href={card.link} className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div
-        className="card"
-        style={{ width: "18rem", flex: "1", margin: "0 10px" }}
-      >
-        <img
-          className="card-img-top"
-          src="/src/files /IMG_0166.HEIC" 
-          alt="Card image cap"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div
-        className="card"
-        style={{ width: "18rem", flex: "1", margin: "0 10px" }}
-      >
-        <img
-          className="card-img-top"
-          src="/src/files /IMG_0166.HEIC"
-          alt="Card image cap"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div
-        className="card"
-        style={{ width: "18rem", flex: "1", margin: "0 10px" }}
-      >
-        <img
-          className="card-img-top"
-          src="/src/files /IMG_0166.HEIC" 
-          alt="Card image cap"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
